cd6e27733cff16f8fa4bce7a6fdd502d0163bd4f
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-10-14 14:54:21 -0700

    Header Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Weather from './images/rain.gif'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Weather Update</h2>
+        <img src={Weather} className='w-image' alt="WeatherUpdate" srcset="" />
+        <hr />
     </div>
   );
 }
